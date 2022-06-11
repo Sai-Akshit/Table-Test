4638597d0e75313d4bbd7ebf8e7a68f97bfa9be8
akshit016 <akshit.uriti@gmail.com>
2022-06-11 16:15:59 +0530

    modified errors

--- timetable/views.py
+++ timetable/views.py
@@ -5,8 +5,28 @@ from . import algorithm
 
 # Create your views here.
 def juniorTable(request):
-    # ttDict = algorithm.main(1)
-    ttDict = {'L4': 'CSEPY', 'L5': 'CSEPY', 'L12': 'CSEPY', 'L8': 'LANG1001', 'L15': 'LANG1001', 'L11': 'MECH1001', 'L16': 'MECH1001', 'L7': 'CHEMP', 'L18': 'ECEEP', 'L14': 'CLAD1041', 'D1': 'ECEETH', 'D2': 'ECEETH', 'D3': 'ECEETH', 'G1': 'CHEMTH', 'G2': 'CHEMTH', 'G3': 'CHEMTH'}
+    ttDict ={
+        'L2': 'CSEPY', 
+        'L10': 'CSEPY', 
+        'L17': 'CSEPY', 
+        'L6': 'LANG1001', 
+        'L13': 'LANG1001', 
+        'L1': 'MECH1001', 
+        'L9': 'MECH1001',
+         'L15': 'CHEMP',
+         'L8': 'ECEEP',
+         'L5': 'CLAD1041',
+         'TD12': 'MATH1041',
+         'TD11': 'MATH1041',
+         'G1': 'ECEETH',
+         'TG11': 'ECEETH',
+         'TG12': 'ECEETH',
+         'TH1': 'CHEMTH',
+         'H2': 'CHEMTH',
+         'H3': 'CHEMTH',
+         'I2': 'MATH1051',
+         'I3': 'MATH1051'
+    }
     return render(request, 'junior-table.html', ttDict)
 
 def seniorTable(request):
